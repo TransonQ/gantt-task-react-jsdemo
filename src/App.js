d31967edd61b4c45a82a04d644954b6a89647883
quanscheng <quanscheng@163.com>
2022-05-12 12:31:13 +0800

    gantt-task-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "gantt-task-react/dist/index.css"
+
+import { Gantt, Task, ViewMode } from "gantt-task-react"
+import React, { useState } from "react"
+import { getStartEndDateForProject, initTasks } from "./Helper.jsx"
+
+import { ViewSwitcher } from "./components/view-switcher"
 
 function App() {
+  const [view, setView] = useState(ViewMode.Day)
+  const [tasks, setTasks] = useState(initTasks())
+  const [isChecked, setIsChecked] = useState(true)
+  let columnWidth = 65
+  if (view === ViewMode.Month) {
+    columnWidth = 300
+  } else if (view === ViewMode.Week) {
+    columnWidth = 250
+  }
+
+  const handleTaskChange = (task) => {
+    console.log("On date change Id:" + task.id)
+    let newTasks = tasks.map((t) => (t.id === task.id ? task : t))
+    if (task.project) {
+      const [start, end] = getStartEndDateForProject(newTasks, task.project)
+      const project = newTasks[newTasks.findIndex((t) => t.id === task.project)]
+      if (project.start.getTime() !== start.getTime() || project.end.getTime() !== end.getTime()) {
+        const changedProject = { ...project, start, end }
+        newTasks = newTasks.map((t) => (t.id === task.project ? changedProject : t))
+      }
+    }
+    setTasks(newTasks)
+  }
+
+  const handleTaskDelete = (task) => {
+    const conf = window.confirm("Are you sure about " + task.name + " ?")
+    if (conf) {
+      setTasks(tasks.filter((t) => t.id !== task.id))
+    }
+    return conf
+  }
+
+  const handleProgressChange = async (task) => {
+    setTasks(tasks.map((t) => (t.id === task.id ? task : t)))
+    console.log("On progress change Id:" + task.id)
+  }
+
+  const handleDblClick = (task) => {
+    alert("On Double Click event Id:" + task.id)
+  }
+
+  const handleSelect = (task, isSelected) => {
+    console.log(task.name + " has " + (isSelected ? "selected" : "unselected"))
+  }
+
+  const handleExpanderClick = (task) => {
+    setTasks(tasks.map((t) => (t.id === task.id ? task : t)))
+    console.log("On expander click Id:" + task.id)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ViewSwitcher
+        onViewModeChange={(viewMode) => setView(viewMode)}
+        onViewListChange={setIsChecked}
+        isChecked={isChecked}
+      />
+      <h3>Gantt With Unlimited Height</h3>
+      <Gantt
+        tasks={tasks}
+        viewMode={view}
+        onDateChange={handleTaskChange}
+        onDelete={handleTaskDelete}
+        onProgressChange={handleProgressChange}
+        onDoubleClick={handleDblClick}
+        onSelect={handleSelect}
+        onExpanderClick={handleExpanderClick}
+        listCellWidth={isChecked ? "155px" : ""}
+        columnWidth={columnWidth}
+      />
+      <h3>Gantt With Limited Height</h3>
+      <Gantt
+        tasks={tasks}
+        viewMode={view}
+        onDateChange={handleTaskChange}
+        onDelete={handleTaskDelete}
+        onProgressChange={handleProgressChange}
+        onDoubleClick={handleDblClick}
+        onSelect={handleSelect}
+        onExpanderClick={handleExpanderClick}
+        listCellWidth={isChecked ? "155px" : ""}
+        ganttHeight={300}
+        columnWidth={columnWidth}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
